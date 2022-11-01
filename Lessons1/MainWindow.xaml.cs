de98e9f419a23a53a1b9938ea2f43d74003aad6e
анастасія <@DESKTOP-BQG54B8>
2022-11-01 18:12:25 +0200

    Add project files.

---
+++ Lessons1/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Lessons1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxOutput.Text = TextBoxInput.Text;
+        }
+    }
+}
